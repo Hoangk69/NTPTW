1acb0b2c4325dafaad3c13946b64ceb04bb37f3b
Huy Hoang <huyhoang9a.bk@gmail.com>
2022-12-12 22:52:08 +0700

    finish header+nav

---
+++ js/home.js
@@ -0,0 +1,43 @@
+const menuAccount = document.getElementById('nav-account');
+const btnAccount = document.getElementById('my-account');
+
+const menuMessenger = document.getElementById('nav-messenger');
+const btnMessenger = document.getElementById('messenger');
+
+const iconMessenger = document.querySelector('.fa-facebook-messenger');
+/* 
+*  handle event Headers
+*/
+// hien thi menu account
+function myAccount(){
+    if(menuAccount.style.display == 'block'){
+        menuAccount.style.display = 'none';
+    }else{
+        menuAccount.style.display = 'block';
+    }
+    //menuAccount.classList.toggle('show-nav-account');
+}
+
+// close click outside
+document.onclick = function(e){
+    if (e.target.id != 'my-account' && e.target.id != 'avt-account' && !$(e.target).parents('#nav-account').length) {
+        console.log('vao day 1');
+        // console.log(btnAccount);
+        console.log(e.target.id);
+        menuAccount.style.display = 'none';
+        //menuAccount.classList.remove('show-nav-account');
+    }
+    if (e.target.id != 'messenger' && e.target.id != 'icon-messenger') {
+        console.log('hello 2' );
+        menuMessenger.classList.remove('show-nav-messenger');
+        iconMessenger.classList.remove('click-color-icon-messsenger');
+        btnMessenger.classList.remove('click-background-btn-messenger');
+    }
+}
+
+// hien thi messenger
+function myMessenger(){
+    menuMessenger.classList.toggle('show-nav-messenger');
+    iconMessenger.classList.toggle('click-color-icon-messsenger');
+    btnMessenger.classList.toggle('click-background-btn-messenger');
+}
